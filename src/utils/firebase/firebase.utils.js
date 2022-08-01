587d41e5bdf8b950d671e511dc66ee467ff968e6
Larrygf02 <raulgf_02@hotmail.com>
2022-07-31 21:52:10 -0500

    Set up firebase auth

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
