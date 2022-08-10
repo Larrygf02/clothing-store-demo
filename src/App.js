f584b22e93857ac1f19afb7840ac0da13745dfc2
Larrygf02 <raulgf_02@hotmail.com>
2022-08-09 23:03:39 -0500

    go to checkout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Authentication from "./routes/authentication/authentication";
+import Checkout from "./routes/checkout/checkout.component";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Shop from "./routes/shop/shop.component";
+
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="shop" element={<Shop />}></Route>
         <Route path="auth" element={<Authentication></Authentication>}></Route>
+        <Route path="checkout" element={<Checkout></Checkout>}></Route>
       </Route>
     </Routes>
   );
