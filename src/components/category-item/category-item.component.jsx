2ffd49b851bb747970f3859d3dc80254dfb80a65
Larrygf02 <raulgf_02@hotmail.com>
2022-08-13 12:53:07 -0500

    navigate to categories

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,16 @@
 import './category-item.styles.scss'
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
+    let navigate = useNavigate()
     const {id, imageUrl, title } = category;
+    const navigateToCategory = (category) => {
+        console.log('algo')
+        navigate(`shop/${category}`)
+    }
+
     return (
-        <div key={id} className="category-container">
+        <div key={id} className="category-container" onClick={() => navigateToCategory(title)}>
             <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className="category-body-container">
                 <h2>{title}</h2>
